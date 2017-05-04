b4280c202c3c3980e11449279076f77b8b83b0e4
Amanda Southworth <amanda.southworth@myds.us>
2017-05-04 12:15:07 -0700

    added more nerd stuff

---
+++ waves/waves.cpp
@@ -0,0 +1,14 @@
+double velocity(double frequency, double wavelength) {
+    
+    return frequency * wavelength; 
+}
+
+double frequency(double velocity, double wavelength) {
+    
+    return; 
+}
+
+double wavelength(double velocity, double frequency) {
+    
+    return;
+}
---
+++ waves/waves.h
@@ -0,0 +1,8 @@
+#ifndef WAVES_H
+#define WAVES_H
+
+double velocity(double frequency, double wavelength);
+double frequency(double velocity, double wavelength);
+double wavelength(double velocity, double frequency);
+
+#endif
